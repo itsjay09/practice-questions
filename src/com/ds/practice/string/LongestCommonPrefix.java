065356d89091cffe2620fedf4b17654ad877a50a
jayeshpatil <jayesh.patil1594@gmail.com>
2022-06-22 08:20:13 +0530

    Practice preblem

---
+++ src/com/ds/practice/string/LongestCommonPrefix.java
@@ -0,0 +1,62 @@
+package com.ds.practice.string;
+
+public class LongestCommonPrefix {
+
+    public static void main(String[] args) {
+        //String[] arr = {"geeksforgeeks", "geeks", "geek", "geezer" };
+        String[] arr = {"apple", "ape", "april" };
+        String s = checkCommonPrefixApp2(arr, arr.length);
+        System.out.println(s);
+    }
+
+    private static String checkCommonPrefixApp2(String[] arr, int n) {
+        int minLength = minLength(arr, n);
+        String strRet = "";
+        char curr;
+        // mim length char iteration
+        for (int i = 0; i < minLength; i++) {
+            curr = arr[0].charAt(i);
+            // String iteration
+            for (int j = 1; j < n; j++) {
+                if (arr[j].charAt(i) != curr) {
+                    return strRet;
+                }
+            }
+            strRet += curr;
+        }
+        return strRet;
+    }
+
+    private static int minLength(String[] arr, int n) {
+        int minLength = arr[0].length();
+        for (int i = 1; i < n; i++) {
+            if (arr[i].length() < minLength) {
+                minLength = arr[i].length();
+            }
+        }
+        return minLength;
+    }
+
+
+    // Approche 1
+    // String by String matching
+    private static String checkCommonPrefix(String[] arr, int n) {
+        String prefix = arr[0];
+        for (int i = 1; i < n; i++) {
+            prefix = checkCommonOfString(arr[i], prefix);
+        }
+        return prefix;
+    }
+
+    private static String checkCommonOfString(String str1, String str2) {
+        int n1 = str1.length(), n2 = str2.length();
+        String strRet = "";
+        for (int i = 0, j = 0; i < n1 && j < n2; i++, j++) {
+            if (str1.charAt(i) != str2.charAt(j)) {
+                break;
+            }
+            strRet += str1.charAt(i);
+        }
+        return strRet;
+    }
+}
--- src/com/ds/practice/string/ValidSubstring.java
+++ src/com/ds/practice/string/ValidSubstring.java
@@ -3,8 +3,26 @@ package com.ds.practice.string;
 public class ValidSubstring {
 
     public static void main(String[] args) {
-
+        String S ="(()(";
+        int maxLen = findMaxLen(S);
+        System.out.println(maxLen);
     }
 
-
+    private static int findMaxLen(String S) {
+        int open = 0, close = 0, max = 0;
+        for (int i = 0; i < S.length(); i++) {
+            if (S.charAt(i) == '(') open++;
+            else close++;
+            if (open == close) max = Math.max(max, open + close);
+            else if(close > open) open = close = 0;
+        }
+        open = close = 0;
+        for (int i = S.length() - 1; i >= 0; i--) {
+            if (S.charAt(i) == '(') open++;
+            else close++;
+            if (open == close) max = Math.max(max, open + close);
+            else if(close < open) open = close = 0;
+        }
+        return max;
+    }
 }
