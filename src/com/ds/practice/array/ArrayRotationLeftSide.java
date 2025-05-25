9c0c92141a123d16c043dea6e23c3dd7ff41010c
jayeshpatil <jayesh.patil1594@gmail.com>
2025-05-25 10:01:58 +0530

    rotation array updated version

--- src/com/ds/practice/array/ArrayRotationLeftSide.java
+++ src/com/ds/practice/array/ArrayRotationLeftSide.java
@@ -9,29 +9,63 @@ public class ArrayRotationLeftSide {
     public static void main(String[] args) {
 
         int[] array = {1, 2, 3, 4, 5};
-        int steps = 4;
+        int steps = 2;
+
+
+        //store last element
+        int lastElement  =  array[array.length  -1];
+
+        // rorate the by one postion
+        for (int i = array.length - 1 ; i > 0 ; i--) {
+            array[i] = array[i -1];
+        }
+
+        array[0] = lastElement;
+
 
         System.out.println("Before :-> " + Arrays.toString(array));
-
-        int[] tempArray = arrayRotationLeftSideByN(array, steps, array.length);
-
-        System.out.println("After :-> " + Arrays.toString(tempArray));
+//        System.out.println("Before :-> " + Arrays.toString(array));
+//
+//        int[] tempArray = arrayRotationLeftSideByN(array, steps, array.length);
+//
+//        System.out.println("After :-> " + Arrays.toString(tempArray));
     }
 
-    private static int[] arrayRotationLeftSideByN(int[] array, int d, int n) {
-        int[] tempArray = new int[n];
-        int length = n;
-        int currIndex = length - 1;
 
-        while (currIndex >= 0) {
-            if ((currIndex - d) < 0) {
-                int indexToAdd = length - (d - currIndex);
-                tempArray[indexToAdd] = array[currIndex];
-            } else {
-                tempArray[currIndex - d] = array[currIndex];
+    public static  void rotatearrayByN2(int[] arr, int rot){
+
+        int[]  tempArray = new int[arr.length];
+        int n = arr.length - 1;
+
+        for (int i = 0; i <= n ; i++ ){
+
+            if(! ((rot - i) >= n) ){
+                tempArray[i + rot ]= arr[i];
+            }else{
+                int remaningRotation = n - i;
+
+                tempArray[remaningRotation ]= arr[i];
             }
-            currIndex--;
+
         }
-        return tempArray;
+
     }
+
+    // Bru force Solution
+    // Time complexity is O(rot * N)
+    public static void rotateArrayByN1(int[] arr, int rot) {
+
+        for (int i = 1; i <= rot; i++) {
+
+            int lastElement = arr[arr.length - 1];
+
+            for (int j = arr.length - 1; j > 0; j--) {
+                arr[j] = arr[j - 1];
+
+            }
+
+            arr[0] = lastElement;
+        }
+    }
+
 }
